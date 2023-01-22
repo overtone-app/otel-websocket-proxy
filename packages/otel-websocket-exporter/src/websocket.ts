aaec42a2c72a6077660a08d2a96cb646090f8464
Johannes Schickling <schickling.j@gmail.com>
2023-01-22 18:55:27 +0100

    inital exporter

---
+++ packages/otel-websocket-exporter/src/websocket.ts
@@ -0,0 +1,30 @@
+export const connectToWebSocket = async (url: string) => {
+  const socket = new WebSocket(url)
+
+  return new Promise<WebSocket>((resolve, reject) => {
+    socket.addEventListener(
+      'message',
+      (msg) => {
+        const data = JSON.parse(msg.data)
+        if (data.type === 'init-response') {
+          resolve(socket)
+        } else {
+          console.log('Unexpected message', data)
+
+          reject(new Error('Unexpected message'))
+        }
+      },
+      { once: true },
+    )
+
+    socket.send(JSON.stringify({ type: 'init-request' }))
+  })
+}
+
+export const sendToWebSocket = (socket: WebSocket, data: string) => {
+  socket.send(JSON.stringify(data))
+}
+
+export const closeWebSocket = (socket: WebSocket) => {
+  socket.close()
+}
