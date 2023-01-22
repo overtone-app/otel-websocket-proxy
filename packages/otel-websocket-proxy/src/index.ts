bb63e173c10d8318a682742e964d7fa234a4a77e
Siddhant <siddhant@fastmail.com>
2023-01-22 17:33:44 +0100

    feat(proxy): Ping pong

--- packages/otel-websocket-proxy/src/index.ts
+++ packages/otel-websocket-proxy/src/index.ts
@@ -1,1 +1,15 @@
-export {}
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 8080 });
+console.log("Listening on", 8080);
+
+server.on("connection", (ws) => {
+  console.log("New connection");
+
+  ws.on("message", (data) => {
+    console.log("Received message", data.toString());
+    ws.send("pong");
+  });
+
+  ws.send("welcome!");
+});
